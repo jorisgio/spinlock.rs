e45b38d837012d1f9b87732de44e84fb913e52d2
Joris Giovannangeli <joris@giovannangeli.fr>
2015-03-09 14:38:32 +0100

    Add doc

---
+++ implementors/core/ops/trait.Not.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['spinlock'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/std/old_io/struct.FilePermission.html' title='std::old_io::FilePermission'>FilePermission</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/str/trait.Str.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['spinlock'] = ["<a class='stability Unstable' title='Unstable: waiting on Str stabilization'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/str/trait.Str.html' title='core::str::Str'>Str</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>","<a class='stability Unstable' title='Unstable: Instead of taking this bound generically, this trait will be replaced with one of slicing syntax (&amp;foo[..]), deref coercions, or a more generic conversion trait'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/str/trait.Str.html' title='core::str::Str'>Str</a> for <a class='enum' href='http://doc.rust-lang.org/nightly/collections/borrow/enum.Cow.html' title='collections::borrow::Cow'>Cow</a>&lt;'a, <a href='http://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
